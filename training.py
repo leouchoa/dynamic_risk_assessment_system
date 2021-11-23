9cdf29830af1583184aee7c1c0cde85ac42c50a0
Leonardo <leonardouchoa.pedreira@gmail.com>
2021-11-23 20:48:24 -0300

    file to train a logistic regresion model on ingestion.py output

---
+++ training.py
@@ -0,0 +1,97 @@
+"""
+Train a logistic regression model on outputed data from ingestion.py
+Input:
+    - ingesteddata/finaldata.csv
+Output:
+    - practicemodels/trainedmodel.pkl
+"""
+import json
+import logging
+import os
+import pickle
+from typing import Tuple
+
+import pandas as pd
+from pandas.core.construction import array
+from sklearn.linear_model import LogisticRegression
+
+# setup logger basic config
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    # datefmt='%a, %d %b %Y %H:%M:%S',
+    filename="logging_results/training.log",
+    filemode="w",
+)
+
+logger = logging.getLogger("training")
+
+
+def load_and_prepare(config: json) -> Tuple[pd.DataFrame, array]:
+    """
+    Reads the configuration file path to ingestion.py output and
+    returns a tuple containing (x,y) pair of training matrix `x` and
+    `y` labels
+    """
+    dataset_csv_path = os.path.join(
+        config["output_folder_path"], "finaldata.csv"
+    )
+    logger.info("Reading finaldata.csv")
+    df = pd.read_csv(dataset_csv_path)
+
+    logger.info("Dropping columns and creating x/y data")
+    df = df.drop("corporation", axis=1)
+    y = df.exited.values
+    x = df.drop("exited", axis=1)
+
+    return (x, y)
+
+
+def train_model(train_x_y: Tuple[pd.DataFrame, array], config: json) -> None:
+    """
+    Reads the output of load_and_prepare and the json config, train a logistic
+    regression model and save it specified output folder in config.
+    """
+    x, y = train_x_y
+
+    lr_mod = LogisticRegression(
+        C=1.0,
+        class_weight=None,
+        dual=False,
+        fit_intercept=True,
+        intercept_scaling=1,
+        l1_ratio=None,
+        max_iter=100,
+        multi_class="auto",
+        n_jobs=None,
+        penalty="l2",
+        random_state=0,
+        solver="liblinear",
+        tol=0.0001,
+        verbose=0,
+        warm_start=False,
+    )
+
+    logger.info("Training model")
+    lr_mod.fit(x, y)
+
+    if not os.path.exists(config["output_model_path"]):
+        os.makedirs(config["output_model_path"])
+
+    model_output_path = os.path.join(
+        config["output_model_path"], "trainedmodel.pkl"
+    )
+
+    logger.info(f"Saving model into {model_output_path}")
+
+    pickle.dump(lr_mod, open(model_output_path, "wb"))
+
+    logger.info("Saving succeded")
+
+
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    res = load_and_prepare(config)
+    train_model(res, config)
